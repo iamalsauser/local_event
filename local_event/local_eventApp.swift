76bd8c2bea340f497565c13e00308cb7c272a0bf
Iamalsauser <parthsinhv4@gmail.com>
2025-01-30 21:25:55 +0530

    Initial Commit

---
+++ local_event/local_eventApp.swift
@@ -0,0 +1,17 @@
+//
+//  local_eventApp.swift
+//  local_event
+//
+//  Created by admin17 on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct local_eventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ local_eventTests/local_eventTests.swift
@@ -0,0 +1,17 @@
+//
+//  local_eventTests.swift
+//  local_eventTests
+//
+//  Created by admin17 on 30/01/25.
+//
+
+import Testing
+@testable import local_event
+
+struct local_eventTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
